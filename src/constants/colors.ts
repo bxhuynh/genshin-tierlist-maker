190f894e971b4907e3bd1d14f025a51e263fad47
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2022-08-04 10:40:01 +0700

    create app data

---
+++ src/constants/colors.ts
@@ -0,0 +1,19 @@
+const colors: string[] = [
+    '#FF7F7F',
+    '#FFBF7F',
+    '#FFDF7F',
+    '#FFFF7F',
+    '#BFFF7F',
+    '#7FFF7F',
+    '#7FFFFF',
+    '#7FBFFF',
+    '#7F7FFF',
+    '#FF7FFF',
+    '#BF7FBF',
+    '#3B3B3B',
+    '#858585',
+    '#CFCFCF',
+    '#F7F7F7',
+];
+
+export { colors };
